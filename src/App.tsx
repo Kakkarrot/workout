b9f1b58e07b61fca84c8c317211899a007716b74
Kakkarrot <lqf5474@gmail.com>
2022-12-20 15:42:37 -0700

    update readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {xuseEffect} from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {Button, Stack, styled} from '@mui/material';
 import {getStyledTextField} from "./components/StyledTextField";
